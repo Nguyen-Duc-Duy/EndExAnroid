a48dd728eaabdf660b81c89e3b478677f7806422
Nguyen-Duc-Duy <duy44204@gmail.com>
2021-08-28 20:50:22 +0700

    first

---
+++ app/src/main/java/com/example/endex/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.endex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
