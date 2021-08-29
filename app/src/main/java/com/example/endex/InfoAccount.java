3adee9c3b5d8dc24cd3e0673575e932edc3d7b79
Nguyen-Duc-Duy <duy44204@gmail.com>
2021-08-29 17:57:22 +0700

    update form account

--- app/src/main/java/com/example/endex/InfoAccount.java
+++ app/src/main/java/com/example/endex/InfoAccount.java
@@ -2,7 +2,9 @@ package com.example.endex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class InfoAccount extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class InfoAccount extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_account);
     }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/endex/MainActivity.java
+++ app/src/main/java/com/example/endex/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.endex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eventLogin(true);
+    }
+    private void eventLogin(Boolean check){
+        if (check){
+            findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this,InfoAccount.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
     }
 }
